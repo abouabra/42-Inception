bbf3a98bd6fb6e340ec79522bb5c5f4abdd0a850
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-10-08 21:58:13 +0100

    bonus: adminer init

--- src/requirements/bonus/adminer/conf/install_adminer.sh
+++ src/requirements/bonus/adminer/conf/install_adminer.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if [ ! -f /var/www/html/wp-config.php ]; then
+if [ ! -f /var/www/html/adminer/index.php ]; then
     mkdir -p /var/www/html/adminer;
-    curl -o /var/www/html/adminer/index.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php;
-    sed -i 's/listen = 127.0.0.1:9000/listen = 0.0.0.0:9000/' /etc/php81/php-fpm.d/www.conf;
+    wget -O /var/www/html/adminer/index.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php;
 else
     echo "adminer is already installed"
 fi
